682bfc19081a3e4f60b3142542a967dc7d67e86b
U-S-MOHANLAL <110254124+U-S-MOHANLAL@users.noreply.github.com>
2023-04-15 12:37:40 +0530

    Update speech_reco.py

--- Intelligent Voice Assistants using OpenAI/speech_reco.py
+++ Intelligent Voice Assistants using OpenAI/speech_reco.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 import openai
 
-openai.api_key =  "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"
+openai.api_key =  "API-KEY"
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
